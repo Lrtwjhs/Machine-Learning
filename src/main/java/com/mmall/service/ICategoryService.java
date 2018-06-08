c893229f4172cdadee55681df3a42a0489f4b434
Lrtwjhs <lrtwjhs@gmail.com>
2018-06-08 14:55:51 +0800

    Add category module

---
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -0,0 +1,21 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
+
+/**
+ * Created by lrtwjhs on 2018/6/7.
+ */
+
+public interface ICategoryService {
+
+    ServerResponse addCategory(String categoryName, Integer parentId);
+
+    ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
+
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
+}
