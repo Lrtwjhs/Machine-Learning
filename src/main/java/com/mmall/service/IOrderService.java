a9868d0adec277872d9e3b47c20589017117f147
Lrtwjhs <lrtwjhs@gmail.com>
2018-06-19 17:40:53 +0800

    add Order module

--- src/main/java/com/mmall/service/IOrderService.java
+++ src/main/java/com/mmall/service/IOrderService.java
@@ -1,8 +1,8 @@
 package com.mmall.service;
 
+import com.github.pagehelper.PageInfo;
 import com.mmall.common.ServerResponse;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.mmall.vo.OrderVo;
 
 import java.util.Map;
 
@@ -14,4 +14,15 @@ public interface IOrderService {
     ServerResponse pay(Long orderNo, Integer userId, String path);
     ServerResponse aliCallback(Map<String,String> params);
     ServerResponse queryOrderPayStatus(Integer userId,Long orderNo);
+    ServerResponse createOrder(Integer userId, Integer shippingId);
+    ServerResponse<String> cancel(Integer userId,Long orderNo);
+    ServerResponse getOrderCartProduct(Integer userId);
+    ServerResponse<OrderVo> getOrderDetail(Integer userId, Long orderNo);
+    ServerResponse<PageInfo> getOrderList(Integer userId, int pageNum, int pageSize);
+
+    //backend
+    ServerResponse<PageInfo> manageList(int pageNum,int pageSize);
+    ServerResponse<OrderVo> manageDetail(Long orderNo);
+    ServerResponse<PageInfo> manageSearch(Long orderNo,int pageNum,int pageSize);
+    ServerResponse<String> manageSendGoods(Long orderNo);
 }
